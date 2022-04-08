84a6b51f79116bd4f31570bfce54de4f5ad0dee1
Samuel <smarouba2@gmail.com>
2022-04-07 22:22:47 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ function writeCards(names) {
 
   for (let i = 0; i < names.length; i++) {
     messeges.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
-    debugger;
+  
   }
 console.log(messeges)
 return messeges
 }
 function countDown (number){
-  var countdown = 10;
-  while (countdown > 0){
-  countdown--;
-  console.log(countdown)
-  console.log("0!");
+  while (number > 0){
+    console.log(number)
+  number-=1;
   }
+  console.log(number);
+}
