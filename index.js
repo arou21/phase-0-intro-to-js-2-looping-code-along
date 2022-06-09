2391b80f085fff318b1dae66312e07c10087766f
Samuel <smarouba2@gmail.com>
2022-06-09 10:28:37 -0400

    commit tree 08c72c1f673428819e6ebb8f30eee6b4ee709f6d

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const names = ["Guadalupe", "Ollie", "Aki",];
 
-function writeCards(names) {
-  const messeges =[]
+ function writeCards(names) {
+   const messeges =[]
 
-  for (let i = 0; i < names.length; i++) {
-    messeges.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+   for (let i = 0; i < names.length; i++) {
+     messeges.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
   
-  }
-console.log(messeges)
-return messeges
-}
-function countDown (number){
+   }
+    console.log(messeges)
+ return messeges
+ }
+ 
+function countDown (number) {
   while (number > 0){
-    console.log(number)
-  number-=1;
+ console.log(number)
+ number-=1;
   }
-  console.log(number);
+  console.log(number)
 }
+
+
